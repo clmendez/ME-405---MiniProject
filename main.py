6341aeb13f390037b1f264ac51062dda059eabf9
clmendez <43482677+clmendez@users.noreply.github.com>
2019-05-20 22:30:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ import encoder
 import motor
 import time
 import utime
+import acc
 
 # Allocate memory so that exceptions raised in interrupt service routines can
 # generate useful diagnostic printouts
@@ -28,37 +29,12 @@ adc = pyb.ADC(pinC0)
 pinC1 = pyb.Pin (pyb.Pin.board.PC1, pyb.Pin.OUT_PP)
 
 def main():
-    global pinC0 
-    global queue
-    global adc
-    global pinC1
-    tim = pyb.Timer(1)   
-    tim.init(freq= 1000)
-    tim.callback(interrupt)
-data
-    while True :
-        pinC1.high ()
-        
-        start_time = utime.ticks_ms()
-        running_time = utime.ticks_ms()
-        
-        while (start_time + 1000) > running_time:
-            running_time = utime.ticks_ms()
-            if queue.empty() == False:
-                print(queue.get())
-        pinC1.low()
-        print("END HEREEEEEEEEEEEEEEEE")
-        time.sleep(30)
+    aye = pyb.I2C(1, pyb.I2C.MASTER)
+    test = acc.Acc (aye, 107)
+    print('Hello')
+    while True:
+        print(test.get_ax_bits())
 
-
-def interrupt(self):
-    global pinC0 
-    global queue
-    global adc
-    global pinC1
-    value = adc.read()
-    if queue.full() == False:
-        queue.put(value)
 # =============================================================================
 
 if __name__ == "__main__":
