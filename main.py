1c6889bdf6ee6b8b5e9c38a09008a79d431a9778
clmendez <43482677+clmendez@users.noreply.github.com>
2019-05-31 18:59:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import time
 import utime
 import filter1
 import acc
-
+import sensor
 # Allocate memory so that exceptions raised in interrupt service routines can
 # generate useful diagnostic printouts
 micropython.alloc_emergency_exception_buf (100)
@@ -35,24 +35,38 @@ def clear():
     print("\x1B\x5BH", end="")
 
 def main():
-    aye = pyb.I2C(1, pyb.I2C.MASTER)
+    control = controller.Controller(1, 5000)
+    
+    motor1 = motor.MotorDriver(pyb.Pin.board.PB4,pyb.Pin.board.PB5, pyb.Pin.board.PA10, pyb.Timer(3))
+    encoder1 = encoder.Encoder(pyb.Pin.board.PB7, pyb.Pin.board.PB6, pyb.Timer(4))
+    
+
+
+
+    
+    
+    '''  aye = pyb.I2C(1, pyb.I2C.MASTER)
     test = acc.Acc (aye, 107)
     filter0 = filter1.Filter(test, .98)
+    sensor0 = sensor.Sensor(pyb.Pin.board.PA8,pyb.Pin.board.PA9)
+    
     while True:
         x_theta = filter0.updated_x()
         y_theta = filter0.updated_y()
-        print("X acceleration raw: ", test.get_ax_int())
-        print("Y acceletation raw: ", test.get_ay_int())
-        print("Z acceletation raw: ", test.get_az_int())
-        print("X acceleration: " + str(test.get_ax()) + " g")
-        print("Y acceletation: " + str(test.get_ay()) + " g")
-        print("Z acceletation: " + str(test.get_az()) + " g")
-        print("updated x: " + str(x_theta))
-        print("updated y: " + str(y_theta))
+        echo = sensor0.measure_distance()
+     #   print("X acceleration raw: ", test.get_ax_int())
+      #  print("Y acceletation raw: ", test.get_ay_int())
+       # print("Z acceletation raw: ", test.get_az_int())
+        #print("X acceleration: " + str(test.get_ax()) + " g")
+        #print("Y acceletation: " + str(test.get_ay()) + " g")
+        #print("Z acceletation: " + str(test.get_az()) + " g")
+        #print("updated x: " + str(x_theta))
+        #print("updated y: " + str(y_theta))
+     #   print("echo: " + str(echo))
         utime.sleep_ms(500)
         
         clear()
-        
+        '''
         
 
 # =============================================================================
