c9908d9df8c6660cd71275319154f1b22f675953
clmendez <43482677+clmendez@users.noreply.github.com>
2019-06-04 09:40:47 -0700

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,9 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 27 16:57:52 2019
+## @file sensor.py
+#  Brief doc for sensor.py
+#
+#  Detailed doc for sensor.py 
+#
+#  @author Anthony Fortner, Claudia Mendez
+#
+#  @copyright License Info
+#
+#  @date June 4, 2019
 
-@author: claud
-"""
 import pyb
 import utime
 import machine
@@ -11,13 +16,23 @@ import machine
 
 class Sensor:
     
+    ## Constructor for Sensor driver
+    #
+    #  Creates a Sensor driver by initializing 
+    #    
+    #  @param pin1 pyb.Pin.board.XXX pin object that connects 
+    #  @param pin2 pyb.Pin.board.XXX pin object that connects
+    
     def __init__(self, pin1, pin2):
+        
         self.echo = pyb.Pin (pin1, pyb.Pin.IN)
+        
         self.trigger = pyb.Pin (pin2, pyb.Pin.OUT)
         
         
-        
-        
+    ## This method gets the distance from noise that the sensor detects.    
+    #  @return distance of noise that the sensor detects
+    
     def measure_distance(self):
         self.trigger.high()
         utime.sleep_us(10)
