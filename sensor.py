ba4989e0837d9d5386d1e0cd3605588981a4c93e
clmendez <43482677+clmendez@users.noreply.github.com>
2019-06-09 21:02:00 -0700

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -18,15 +18,17 @@ class Sensor:
     
     ## Constructor for Sensor driver
     #
-    #  Creates a Sensor driver by initializing 
+    #  Creates a Sensor driver by initializing the pins required to interact with the ultrasonic sensor. 
     #    
     #  @param pin1 pyb.Pin.board.XXX pin object that connects 
     #  @param pin2 pyb.Pin.board.XXX pin object that connects
     
     def __init__(self, pin1, pin2):
         
+        ## Echo is the pulse that we recieve from the ultrasonic sensor 
         self.echo = pyb.Pin (pin1, pyb.Pin.IN)
         
+        ## Trigger is a pulse that we send to the ultrasonic sensor 
         self.trigger = pyb.Pin (pin2, pyb.Pin.OUT)
         
         
@@ -38,6 +40,7 @@ class Sensor:
         utime.sleep_us(10)
         self.trigger.low()
         pulse_time = machine.time_pulse_us(self.echo,1, 50000)
+        print(pulse_time)
         return pulse_time
         
         
